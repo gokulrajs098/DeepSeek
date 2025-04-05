2e44b907ececc7162ddfb4083d3ccaa0ebd430b3
Gokul Raj <serajgokul19@gmail.com>
2025-04-06 00:08:30 +0530

    finished implementing cache system and halfway through the forward pass

--- deepseek.py
+++ deepseek.py
@@ -1,5 +1,4 @@
 class MLA(nn.Module):
-    
     """
         MLA(Multi-Headed-Attention-Layer) Attributes:
 
@@ -31,6 +30,51 @@ class MLA(nn.Module):
             self.wq_a = Linear(self.dim, self.q_lora_rank)
             self.q_norm = RMSNorm(self.q_lora_rank)
             self.wq_b = ColumnParallelLinear(self.q_lora_rank, self.n_heads*self.qk_head_dim)
+        self.wkv_a = Linear(self.dim, self.kv_lora_rank + self.qk_rope_head_dim)
+        self.kv_norm = RMSNorm(self.kv_lora_rank)
+        self.wkv_b = ColumnParallelLinear(self.kv_lora_rank, self.n_heads*(self.qk_nope_head_dim+self.v_head_dim))
+        self.wo = RowParallelLinear(self.n_heads*self.v_head_dim, self.dim)
+        self.softmax_scale = self.qk_head_dim ** -0.5
+        if args.max_seq_len > args.original_seq_len:
+            mscale = 0.1 * args.mscale * math.log(args.rope_factor) + 1.0
+            self.softmax_scale = self.softmax_scale * mscale * mscale
+        if attn_impl == "naive":
+            self.register_buffer("k_cache", torch.zeros(args.max_batch_size, args.max_seq_len, self.n_local_heads, self.qk_head_dim), persistent=False)
+            self.register_buffer("v_cache", torch.zeros(args.max_batch_size, args.max_seq_len, self.n_local_heads, self.v_head_dim), persistent=False)
+        else:
+            self.register_buffer("kv_cache", torch.zeros(args.max_batch_size, args.max_seq_len, self.kv_lora_rank), persistent=False)
+            self.register_buffer("pe_cache", torch.zeros(args.max_batch_size, args.max_seq_len, self.qk_rope_head_dim), persistent=False)
+
+    def forward(self, x: torch.Tensor, start_pos: int, freqs_cis: torch.Tensor, mask: Optional(torch.Tensor)):
+        """
+        Forward  pass for Multi-headed Attention Layer(MLA).
+
+        Args:
+            x(torch.Tensor): Input tensor of shape(batch_size, seq_len, dim).
+            start_pos(int): starting position in the sequence for caching.
+            freqs_cis(torch.Tensor): PreComputed complex exponential values for rotary embedings.
+            mask(Optional[torch.Tensor]): Mask tensor to exclude certain positions from attention.
+
+        Returns:
+            torch.Tensor: Output tensor with the same shape as the input
+        """
+
+        bsz, seqlen = x.size()
+        end_pos = start_pos + seqlen
+        if self.q_lora_rank == 0:
+            q = self.wq(x)
+        else:
+            q = self.wq_b((self.q_norm(self.wq_a(x))))
+        q = q.view(bsz, seqlen, self.n_heads, self.qk_head_dim)
+        q_nope, q_rope = torch.split(q, [self.qk_nope_head_dim, self.qk_rope_head_dim], dim=-1)
+        q_pe = apply_rotary_emb(q_pe, freqs_cis)
+
+        kv = self.wkv_a(x)
+        kv, k_pe = torch.split(kv, [self.kv_lora_rank, self.qk_rope_head_dim], dim=-1)
+        k_pe = apply_rotary_emb(k_pe.unsquueze(2), freqs_cis)
+        
+
+
 
 
 class ColumnParallelLinear(Linear):
@@ -43,7 +87,6 @@ class ColumnParallelLinear(Linear):
             bias(bool): whether to include a bias term (defaults to false),
             data_type(optional): Data type for the layer (defaults to 'torch.bfloat16').
     """
-
 
     def __init__(self, in_features: int, out_features: int, bias: bool = False, dtype = None):
         assert out_features % world_size == 0, f"Output features must be divisible by world_size"
